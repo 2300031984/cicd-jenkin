ca2207ce78ff45798c14092a0d3891efd5c21904
2300031984 <2300031984@kluniversity.in>
2025-08-06 10:32:58 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-       <h1>Hello, Welcome to CICD</h1>
+       <h1>Hello, Welcome to CICD CI/CD with Vite and React!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
